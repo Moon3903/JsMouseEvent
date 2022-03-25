698c127068adeb929f808d82ad627343757b8852
Zulfiqar Fauzul Akbar <zulfiqar.akbar@gdn-commerce.com>
2022-03-25 14:06:20 +0700

    done

--- script.js
+++ script.js
@@ -5,14 +5,45 @@ var moving = false;
 dog.addEventListener("mousedown", initialClick, false);
 cat.addEventListener("mousedown", initialClick, false);
 window.addEventListener("mouseup", mouseUp, false);
-dog.addEventListener("touchstart", initialClick, false);
-window.addEventListener("touchend", mouseUp, false);
-cat.addEventListener("touchstart", initialClick, false);
-window.addEventListener("touchend", mouseUp, false);
+dog.addEventListener("touchstart", initialTouch, false);
+cat.addEventListener("touchstart", initialTouch, false);
+window.addEventListener("touchend", touchUp, false);
+
+function touchMove(e) {
+  if (e.changedTouches[0]) {
+    var newX = e.changedTouches[0].clientX - 750;
+    var newY = e.changedTouches[0].clientY - 750;
+
+    console.log(newX);
+
+    image.style.left = newX + "px";
+    image.style.top = newY + "px";
+  }
+}
+
+function touchUp() {
+  console.log("masuk touch");
+  // document.removeEventListener("mousemove", move);
+  document.removeEventListener("touchmove", touchMove, false);
+  moving = !moving;
+  return;
+}
+
+function initialTouch(e) {
+  if (moving) {
+    return;
+  }
+
+  moving = !moving;
+  image = this;
+
+  document.addEventListener("touchmove", touchMove, false);
+  console.log("tii");
+}
 
 function move(e) {
-  var newX = e.changedTouches[0].clientX - 10;
-  var newY = e.changedTouches[0].clientY - 10;
+  var newX = e.clientX - 750;
+  var newY = e.clientY - 750;
 
   console.log(newX);
 
@@ -23,22 +54,20 @@ function move(e) {
 function mouseUp() {
   console.log("masuk");
   document.removeEventListener("mousemove", move);
-  document.removeEventListener("touchmove", move, false);
+  // document.removeEventListener("touchmove", move, false);
   moving = !moving;
   return;
 }
 
 function initialClick(e) {
-
-  if(moving){
+  if (moving) {
     return;
   }
-  
+
   moving = !moving;
   image = this;
 
   document.addEventListener("mousemove", move, false);
-  document.addEventListener("touchmove", move, false);
-  console.log("sii")
+  // document.addEventListener("touchmove", move, false);
+  console.log("sii");
 }
-
